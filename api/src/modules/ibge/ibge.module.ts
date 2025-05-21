8472366f825a6c9a07a41323343f69e9b79be9da
oluquejs <luquedevjs@icloud.com>
2025-05-21 15:09:30 -0300

    chore(app): identation fix

--- api/src/modules/ibge/ibge.module.ts
+++ api/src/modules/ibge/ibge.module.ts
@@ -1,12 +1,12 @@
-import { HttpModule } from "@nestjs/axios";
-import { Module } from "@nestjs/common";
-import { IbgeService } from "./ibge.service";
-import { IbgeController } from "./ibge.controller";
+import { HttpModule } from '@nestjs/axios';
+import { Module } from '@nestjs/common';
+import { IbgeService } from './ibge.service';
+import { IbgeController } from './ibge.controller';
 
 @Module({
-    imports: [HttpModule],
-    exports: [],
-    providers: [IbgeService],
-    controllers: [IbgeController],
+  imports: [HttpModule],
+  exports: [],
+  providers: [IbgeService],
+  controllers: [IbgeController],
 })
 export class IbgeModule {}
