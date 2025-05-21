9b6dde69ccae7f98152d24597b2ed839133c10db
oluquejs <luquedevjs@icloud.com>
2025-05-21 15:50:11 -0300

    feat(jwt): create auth controller

---
+++ api/src/modules/auth/auth.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { CreateUserDto } from '../user/dto/create-user.dto';
+import { UserService } from '../user/user.service';
+import * as bcrypt from 'bcrypt';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
+
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+
+  @Post('register')
+  async register(@Body() createUserDto: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 13);
+    
+    const user = await this.userService.createUser({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+
+    return user;
+  }
+}
