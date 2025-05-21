577c76d4e738f2599f33d59b6c8ae4bb4d6261b8
oluquejs <luquedevjs@icloud.com>
2025-05-21 15:55:42 -0300

    feat(auth): add AuthModule to AppModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,6 +3,7 @@ import { IbgeModule } from './modules/ibge/ibge.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './common/database/database.module';
 import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { UserModule } from './modules/user/user.module';
     IbgeModule,
     DatabaseModule,
     UserModule,
+    AuthModule
   ],
   exports: [],
 })
